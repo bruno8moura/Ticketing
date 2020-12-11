b11a9c7f2ac037a97cd86af7a6e2d00a42e50208
Bruno Moura <bruno8moura@gmail.com>
2020-12-11 05:34:35 -0300

    Mongdb configured inside cluster.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import currentUserRouter from './modules/currentuser/routes';
 import signinRouter from './modules/signin/routes';
@@ -12,12 +13,6 @@ import NotFoundError from './shared/errors/NotFoundError';
 const app = express();
 app.use(json());
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!');
-});
-
-app.get('/', (request: Request, response: Response) => response.status(200).json({ok: true}));
-
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
@@ -28,5 +23,23 @@ app.all('*', async () => {
 })
 
 app.use(errorHandler);
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        
+        app.listen(3000, () => {
+            console.log('Listening on port 3000!');
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+start();
 
 export default app;
