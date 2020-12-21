bf8448a457a85ebf2168a1c7d9d28a1ff048de35
Bruno Moura <bruno8moura@gmail.com>
2020-12-21 06:20:16 -0300

    Refactory: index.ts separated in two distincts files.

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import 'express-async-errors';
+import express from 'express';
+import { json } from 'body-parser';
+
+import cookieSession from 'cookie-session';
+
+import currentUserRouter from './modules/user/infra/http/routes/currentuser.routes';
+import signinRouter from './modules/user/infra/http/routes/signin.routes';
+import signoutRouter from './modules/user/infra/http/routes/signout.routes';
+import signUpRouter from './modules/user/infra/http/routes/signup.routes';
+import errorHandler from "./shared/middlerwares/errors";
+import NotFoundError from './shared/errors/NotFoundError';
+
+const app = express();
+app.set('trust proxy', true); // tell to express that it is behind a trusted proxy
+app.use(json());
+app.use(
+    cookieSession({
+       signed: false, // the cookie won't be encrypted
+       secure: true,  // use cookies only with https connection
+    })
+)
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signUpRouter);
+
+app.all('*', async () => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,7 @@
-import 'express-async-errors';
-import express from 'express';
-import { json } from 'body-parser';
+import { app } from './app';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import currentUserRouter from './modules/user/infra/http/routes/currentuser.routes';
-import signinRouter from './modules/user/infra/http/routes/signin.routes';
-import signoutRouter from './modules/user/infra/http/routes/signout.routes';
-import signUpRouter from './modules/user/infra/http/routes/signup.routes';
-import errorHandler from "./shared/middlerwares/errors";
-import NotFoundError from './shared/errors/NotFoundError';
-import DatabaseError from './shared/errors/DatabaseError';
 import JwtSecretNotDefinedError from './shared/errors/JwtSecretNotDefinedError';
-
-const app = express();
-app.set('trust proxy', true); // tell to express that it is behind a trusted proxy
-app.use(json());
-app.use(
-    cookieSession({
-       signed: false, // the cookie won't be encrypted
-       secure: true,  // use cookies only with https connection
-    })
-)
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signUpRouter);
-
-app.all('*', async () => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import DatabaseError from './shared/errors/DatabaseError';
 
 const start = async () => {
     if(!process.env.JWT_KEY){
