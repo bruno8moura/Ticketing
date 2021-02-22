f04533bb4fc615a02c507b2660c93182fc70bda6
Bruno Moura <bruno8moura@gmail.com>
2021-02-22 06:21:56 -0300

    All order's routes created.

--- orders/src/modules/orders/infra/http/controllers/OrdersController.ts
+++ orders/src/modules/orders/infra/http/controllers/OrdersController.ts
@@ -5,3 +5,18 @@ export const index = async (req: Request, res: Response) => {
     const tickets = await Ticket.find({});
     return res.send(tickets);
 };
+
+export const del = async (req: Request, res: Response) => {
+    const tickets = await Ticket.find({});
+    return res.send(tickets);
+};
+
+export const show = async (req: Request, res: Response) => {
+    const tickets = await Ticket.find({});
+    return res.send(tickets);
+};
+
+export const create = async (req: Request, res: Response) => {
+    const tickets = await Ticket.find({});
+    return res.send(tickets);
+};
--- orders/src/modules/orders/infra/http/routes/orders.routes.ts
+++ orders/src/modules/orders/infra/http/routes/orders.routes.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-import { index } from '../controllers/OrdersController';
+import { index, del, show, create } from '../controllers/OrdersController';
 
 const routes = express.Router();
 
+routes.post('/', create);
 routes.get('/', index);
+routes.get('/:orderId', show);
+routes.delete('/:orderId', del);
 
 export default routes;
