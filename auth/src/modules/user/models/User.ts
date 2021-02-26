4e239c17f81383f2c98171506be2c80201cbd895
Bruno Moura <bruno8moura@gmail.com>
2021-02-26 05:44:14 -0300

    Some clarification why we create some pieces of code.

--- auth/src/modules/user/models/User.ts
+++ auth/src/modules/user/models/User.ts
@@ -1,15 +1,42 @@
 import mongoose from 'mongoose';
 import { Password } from "../../../shared/utils/hash/Password";
 
+/**
+ * Whenever we are working with typescript and mongoose together
+ * a good way to do this is write this 
+ * 3 interfaces(UserAttrs, UserModel and UserDoc) on top of the file.
+ * 
+ * We do that in order to typescript do some validations when building a new object.
+ */
+
+/** 
+ * An interface that describes the properties
+ * that are required to create a new User.
+ */
 interface UserAttrs {
     email: string;
     password: string;
 }
 
+/**
+ * An interface that describes the properties
+ * that a User Model has.
+ * 
+ * An instance of Model is called Document.
+ * 
+ * The model is the way to interact with the database. 
+ * When we are creating an object, with typescript 
+ * we can validate the fields overwriting the build method
+ * setting an custom interface to it.
+ */
 interface UserModel extends mongoose.Model<UserDoc> {
     build(attrs: UserAttrs): UserDoc;
 }
 
+/**
+ * An interface that describes the properties that User Document has. 
+ * Describes which properties the saved document has. 
+ */
 export interface UserDoc extends mongoose.Document {
     email: string;
     password: string;
