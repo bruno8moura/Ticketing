6495e2501979aa13f7abcad7e09b6830d3a1d37f
Bruno Moura <bruno8moura@gmail.com>
2021-04-01 06:31:10 -0300

    Listening events.

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -3,6 +3,7 @@ import { JwtSecretNotDefinedError, DatabaseError, StreamingServerError } from '@
 import { JWT_KEY, PORT } from './env_variables';
 import { connect as connectToMongoDB } from './shared/infra/connect/mongodb';
 import { connect as connectToNATSStreamServer } from './shared/infra/connect/nats';
+import { listenEvents } from './shared/infra/StreamServerListeners';
 
 const start = async () => {    
     if(!JWT_KEY){
@@ -11,6 +12,7 @@ const start = async () => {
 
     await connectToMongoDB();
     await connectToNATSStreamServer();
+    await listenEvents();
 
     app.listen(PORT, () => {
         console.log('Listening on port ', PORT);
